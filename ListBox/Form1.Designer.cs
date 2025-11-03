803c0218fec64f63abb10a60326fef2731d89373
Leon <konto1chat2345@email.com>
2025-11-03 17:43:51 +0100

    3lis202517.43

--- ListBox/Form1.Designer.cs
+++ ListBox/Form1.Designer.cs
@@ -29,7 +29,12 @@
         private void InitializeComponent()
         {
             this.FruitslistBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // FruitslistBox
@@ -49,32 +54,96 @@
             this.FruitslistBox.Size = new System.Drawing.Size(120, 384);
             this.FruitslistBox.TabIndex = 0;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(198, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Add Items :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(492, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(370, 26);
+            this.textBox1.TabIndex = 2;
+            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(722, 122);
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(492, 103);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Size = new System.Drawing.Size(370, 50);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Green;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(492, 205);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(370, 71);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Count Items";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(492, 312);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(370, 83);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Sort";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(435, 453);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(907, 450);
+            this.ClientSize = new System.Drawing.Size(907, 471);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.FruitslistBox);
             this.Name = "Form1";
             this.Text = "My Textobx Control";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.ListBox FruitslistBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- ListBox/Form1.cs
+++ ListBox/Form1.cs
@@ -16,5 +16,35 @@ namespace ListBox
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FruitslistBox.Items.Add(textBox1.Text);
+
+            textBox1.Clear();
+
+            //FruitslistBox.Focus();
+            textBox1.Focus();
+
+            /*ChatGPT powiedział:
+            W C# metoda Focus() służy do ustawienia fokusu (aktywnego elementu interfejsu)
+            na danym kontrolce — czyli sprawia, że to właśnie ten element będzie aktualnie
+            „aktywny” i gotowy do interakcji z użytkownikiem 
+            (np. do wpisywania tekstu z klawiatury).*/
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int liczba = FruitslistBox.Items.Count;
+            //MessageBox.Show("Liczba wynosi :" + liczba);
+            MessageBox.Show("Liczba wynosi : " + liczba.ToString());
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FruitslistBox.Sorted = true;
+        }
     }
 }
